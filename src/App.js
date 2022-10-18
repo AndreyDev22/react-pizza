0df4daa69487dc40c74db16aea2bb10281c40804
Andrey Ishchenko <andreyishchenko@Elenas-MacBook-Pro.local>
2022-10-18 17:05:02 +0300

    add redux-toolkit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import './scss/app.scss';
 import Header from './components/Header';
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Cart from './pages/Cart';
+
 import { Routes, Route } from 'react-router-dom';
+import { createContext, useState } from 'react';
+
+import './scss/app.scss';
+
+export const SearchContext = createContext();
 
 function App() {
+  const [searchValue, setSearchValue] = useState('');
+
   return (
     <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/cart" element={<Cart />}/>
-          <Route path="*" element={<NotFound />}/>
-        </Routes>
-      </div>
-    </div>
+      <SearchContext.Provider value={{searchValue, setSearchValue}}>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/cart" element={<Cart />}/>
+            <Route path="*" element={<NotFound />}/>
+          </Routes>
+        </div>
+      </SearchContext.Provider>
+    </div> 
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,55 +1,66 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 
 import Categories from '../components/Categories';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 import Sort from '../components/Sort';
+import Pagination from '../components/Pagination';
+import { setCategoryId } from '../redux/slices/FilterSlice';
+import { SearchContext } from '../App';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
 
 const Home = () => {
-    const [items, setItems] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [categoryId, setCategoryId] = useState(0);
-    const [sortType, setSortType] = useState({
-        name: 'популярности',
-        sortProperty: 'rating'
-    });
+  const dispatch = useDispatch();
+  const categoryId = useSelector((state) => state.filter.categoryId);
+  const sortType = useSelector((state) => state.filter.sort.sortProperty);
 
-    useEffect(() => {
-        setIsLoading(true);
+  const { searchValue } = useContext(SearchContext);
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
 
-        const sortBy = sortType.sortProperty.replace('-', '');
-        const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
-        const category = categoryId > 0 ? `category=${categoryId}` : '';
+  const onChangeCategory = (id) => {
+    dispatch(setCategoryId(id));
+  };
 
-        fetch(
-            `https://6342ab413f83935a78470e34.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
-        )
-            .then(res => res.json())
-            .then((arr) => {
-            setItems(arr);
-            setIsLoading(false);
-            });
-        window.scrollTo(0, 0);
-    }, [categoryId, sortType])
+  useEffect(() => {
+    setIsLoading(true);
 
-    return (
-        <>
-            <div className='container'>
-                <div className="content__top">
-                    <Categories value={categoryId} onClickCategory={(index) => setCategoryId(index)}/>
-                    <Sort value={sortType} onClickSortType={(i) => setSortType(i)}/>
-                </div>
-                <h2 className="content__title">Все пиццы</h2>
-                <div className="content__items">
-                    {
-                    isLoading
-                        ? [...new Array(6)].map((_, index) => <Skeleton key={index}/>)
-                        : items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza}/>)
-                    }
-                </div>
-            </div>
-        </>
-    )
-}
+    const sortBy = sortType.replace('-', '');
+    const order = sortType.includes('-') ? 'asc' : 'desc';
+    const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const search = searchValue > 0 ? `&search=${searchValue}` : '';
 
-export default Home
+    axios
+      .get(
+        `https://6342ab413f83935a78470e34.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
+      )
+      .then((response) => {
+        setItems(response.data);
+        setIsLoading(false);
+      });
+
+    window.scrollTo(0, 0);
+  }, [categoryId, sortType, currentPage, searchValue]);
+
+  // const pizzas = items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza}/>);
+  const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
+
+  return (
+    <>
+      <div className="container">
+        <div className="content__top">
+          <Categories value={categoryId} onClickCategory={onChangeCategory} />
+          <Sort />
+        </div>
+        <h2 className="content__title">Все пиццы</h2>
+        <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+        <Pagination onChangePage={(number) => setCurrentPage(number)} />
+      </div>
+    </>
+  );
+};
+
+export default Home;
